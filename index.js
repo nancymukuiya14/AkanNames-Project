7489328c4d8713366ff0df5c3333af16d067afb1
nancy.kigotho@student.moringaschool.com <nancykigotho14@gmail.com>
2021-07-18 17:11:13 +0300

    modified index.html

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+
 var btn=document.getElementById("submit1");
+
+//adding event listener on the click button//
+
 btn.addEventListener("click", function () { 
     // alert ("hello");
     var year= Number(document.getElementById("year").value);
     var month=Number (document.getElementById("month").value);
     var date= Number(document.getElementById("date").value);
     var gender=document.querySelector('input[name="gender"]:checked');
+    //control structer//
     if (date<=0 || date>31){
         alert ("Enter valid date of birth");
     }
