17a197a785dbbda4b87945fbf6204c01df5532d1
nancy.kigotho@student.moringaschool.com <nancykigotho14@gmail.com>
2021-07-18 17:26:15 +0300

    add a comments on the index.js file

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 
-var btn=document.getElementById("submit1");
+var btn = document.getElementById("submit1");
 
 //adding event listener on the click button//
 
-btn.addEventListener("click", function () { 
+btn.addEventListener("click", function () {
     // alert ("hello");
-    var year= Number(document.getElementById("year").value);
-    var month=Number (document.getElementById("month").value);
-    var date= Number(document.getElementById("date").value);
-    var gender=document.querySelector('input[name="gender"]:checked');
+    var year = Number(document.getElementById("year").value);
+    var month = Number(document.getElementById("month").value);
+    var date = Number(document.getElementById("date").value);
+    var gender = document.querySelector('input[name="gender"]:checked');
+
     //control structer//
-    if (date<=0 || date>31){
-        alert ("Enter valid date of birth");
+    if (date <= 0 || date > 31) {
+        alert("Enter valid date of birth");
     }
-    else if (month<=0 || month>12){
-        alert ("Enter valid month of birth");
+    else if (month <= 0 || month > 12) {
+        alert("Enter valid month of birth");
     }
-    else if (year<=1900 || year>2100){
-        alert ("Enter valid year of birth");
+    else if (year <= 1900 || year > 2100) {
+        alert("Enter valid year of birth");
 
     }
-   else if (gender==null) {
-   alert ("Select your gender");
-}
-let boys = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
-let girls = [" Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
-let days = ["sunday", "monday", "teusday", "wednesday", "thursday", "friday", "saturday"]
-let fullBirthday=year + "-"+ month + "-" + date;
-let dayvalue =new Date(fullBirthday);
-console.log(dayvalue);
-let dayOfTheWeek =dayvalue.getDay()
-console.log(dayOfTheWeek);
+    else if (gender == null) {
+        alert("Select your gender");
+    }
 
-if (gender.value=="male") {
-    alert (`Your Akan Name is ${boys[dayOfTheWeek]}. You were born on a ${days[dayOfTheWeek]} `)
-}
-else if (gender.value=="female") {
-    alert (`Your Akan Name is ${girls[dayOfTheWeek]}. You were born on a ${days[dayOfTheWeek]} `)
-}
+    let boys = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+    let girls = [" Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+    let days = ["sunday", "monday", "teusday", "wednesday", "thursday", "friday", "saturday"]
+    let fullBirthday = year + "-" + month + "-" + date;
+    let dayvalue = new Date(fullBirthday);
+    console.log(dayvalue);
+    let dayOfTheWeek = dayvalue.getDay()
+    console.log(dayOfTheWeek);
+    //Getting the Akan Name//
 
-
-     
+    if (gender.value == "male") {
+        alert(`Your Akan Name is ${boys[dayOfTheWeek]}. You were born on a ${days[dayOfTheWeek]} `)
+    }
+    else if (gender.value == "female") {
+        alert(`Your Akan Name is ${girls[dayOfTheWeek]}. You were born on a ${days[dayOfTheWeek]} `)
+    }
 })
-    
-    
+
+
